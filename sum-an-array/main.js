bc4aa0294f6f84de6f39757db51edbba82e4b96d
Matthew Chen <matthewchen@Matthews-MacBook-Pro.local>
2020-07-28 10:19:13 -0700

    Completed sum an array

---
+++ sum-an-array/main.js
@@ -0,0 +1,9 @@
+
+var arrayIndex = 0;
+var numbersArray = [];
+function sumArray(numbersArray) {
+  for (var i = 0; i < numbersArray.length; i++) {
+    arrayIndex += numbersArray[i];
+
+  } return arrayIndex;
+}
