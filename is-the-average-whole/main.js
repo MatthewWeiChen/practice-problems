10f23581fc6808dd5eec8ecbb44885ba0957e9d3
Matthew Chen <matthewchen@Matthews-MacBook-Pro.local>
2020-08-03 10:48:15 -0700

    revised

--- is-the-average-whole/main.js
+++ is-the-average-whole/main.js
@@ -5,7 +5,6 @@ function isTheAverageWhole(numbersArray) {
   for (var i = 0; i < numbersArray.length; i++) {
     temp += numbersArray[i];
 
-  } if (temp % 2) {
-    return true;
-  } return false;
+  } var average = temp / numbersArray.length;
+  return Number.isInteger(average);
 }
